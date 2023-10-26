6cc25528952a84af4b28b3837cb998acfe389702
Mónica P. Arroyo <monicaparroyo7@gmail.com>
2023-10-26 10:35:22 -0600

    Configuracion app y document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,40 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react';
+import Document, {
+	Html,
+	Head,
+	Main,
+	NextScript,
+	DocumentContext,
+} from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+import i18nextConfig from '../../next-i18next.config.js';
+
+class MyDocument extends Document {
+	static async getInitialProps(ctx: DocumentContext) {
+		const initialProps = await Document.getInitialProps(ctx);
+		return {
+			...initialProps,
+			styles: React.Children.toArray([initialProps.styles]),
+		};
+	}
+	render(): JSX.Element {
+		return (
+			<Html lang="es">
+				<Head>
+					<meta
+						name="keywords"
+						content="Portfolio, Web Developer, React, PHP, Arduino, Nextjs, Laravel Developer, React Developer, IOT"
+					/>
+					<meta name="author" content="Mónica P. Arroyo" />
+					<link rel="icon" href="/favicon.ico" />
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
+
+export default MyDocument;
